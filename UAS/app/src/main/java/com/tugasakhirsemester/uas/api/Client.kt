b0c330bf349903631deee37985aac5ac30cc43bd
RafidIlham05 <73115097+RafidIlham05@users.noreply.github.com>
2021-01-15 20:28:14 +0700

    Update Client.kt

--- UAS/app/src/main/java/com/tugasakhirsemester/uas/api/Client.kt
+++ UAS/app/src/main/java/com/tugasakhirsemester/uas/api/Client.kt
@@ -3,7 +3,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Client {
-    private const val BASE_URL = "https://api.kawalcorona.com/"
+    private const val BASE_URL = "https://api.kawalcorona.com/" //untuk menampung base url agar dapat dipanggil dimana saja 
 
     val instance: Api by lazy {
         val retrofit = Retrofit.Builder()
